f5689acf348d4702a51ab1636bdf1debd021ac6e
Elkinssm <elkinsilvamanrique@gmail.com>
2021-10-07 22:16:43 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Perfil from "./componentes/Perfil";
 
 function App() {
+  const [nombre, cambiarNombre] = useState("No tiene nombre");
+
+  //Se dispara cada vez que el usuario cambia el texto
+  function eventoCajaTexto(e) {
+    cambiarNombre(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hola Mundo! {nombre}</h1>
+      <input
+        name="nombre"
+        type="text"
+        value={nombre}
+        onChange={eventoCajaTexto}
+      ></input>
+      <Perfil atributomio={nombre} />
+      <Perfil atributomio={nombre} />
+      <Perfil atributomio={nombre} />
+      <Perfil atributomio={nombre} />
     </div>
   );
 }
---
+++ src/componentes/Perfil.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+function Perfil(props) {
+  useEffect(() => {
+    document.title = props.atributomio;
+  }, [props.atributomio]);
+
+  return (
+    <div style={{ background: "yellow" }}>
+      Este es mi nuevo componente Perfil{props.atributomio}
+    </div>
+  );
+}
+
+export default Perfil;
